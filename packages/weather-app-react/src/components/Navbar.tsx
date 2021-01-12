83879efef0dd54977f2a838da2694a3f540102c3
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-01-12 15:29:43 +0100

    add active classes to links

--- packages/weather-app-react/src/components/Navbar.tsx
+++ packages/weather-app-react/src/components/Navbar.tsx
@@ -1,22 +1,22 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { ReactComponent as HomeIcon } from "../assets/icons/home.svg";
 import { ReactComponent as CalendarIcon } from "../assets/icons/calendar.svg";
 
 export const Navbar = () => {
   return (
     <nav>
-      <Link to="/" className="link">
+      <NavLink to="/" className="link" activeClassName="link--active" exact>
         <div className="link__icon">
           <HomeIcon />
         </div>
         <span className="link__text">Home</span>
-      </Link>
-      <Link to="/about" className="link">
+      </NavLink>
+      <NavLink to="/about" className="link" activeClassName="link--active">
         <span className="link__text">Daily</span>
         <div className="link__icon">
           <CalendarIcon />
         </div>
-      </Link>
+      </NavLink>
     </nav>
   );
 };
