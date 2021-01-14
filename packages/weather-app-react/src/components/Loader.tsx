93fb21259772f13258d344947ff57283fe6d8bf1
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-01-14 16:36:26 +0100

    add loader

--- packages/weather-app-react/src/components/Loader.tsx
+++ packages/weather-app-react/src/components/Loader.tsx
@@ -1,7 +1,12 @@
-import { useContext } from "react";
-import { WeatherContext } from "./WeatherContext";
-
 export const Loader = () => {
-  const { loading } = useContext(WeatherContext);
-  return loading ? <div>Loading...</div> : null;
+  return (
+    <div className="loader">
+      <div className="loader__ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
--- packages/weather-app-react/src/views/Home.tsx
+++ packages/weather-app-react/src/views/Home.tsx
@@ -6,29 +6,22 @@ import { HourlyWeather } from "../components/home/HourlyWeather";
 import { WeatherContext } from "../components/WeatherContext";
 
 export const Home = () => {
-  const { currentWeather, dailyWeather, timezone, loading } = useContext(
-    WeatherContext
+  const { currentWeather, dailyWeather, timezone } = useContext(WeatherContext);
+  return (
+    <div className="home">
+      <MainInfo
+        iconCode={currentWeather.weather[0].icon}
+        city={currentWeather.name}
+        country={timezone.countryName}
+        temperature={currentWeather.main.temp}
+      />
+      <HourlyWeather weather={dailyWeather.hourly} time={timezone} />
+      <AdditionalInfo
+        cloudiness={currentWeather.clouds.all}
+        pressure={currentWeather.main.pressure}
+        wind={currentWeather.wind.speed}
+        humidity={currentWeather.main.humidity}
+      />
+    </div>
   );
-
-  if (!loading) {
-    return (
-      <div className="home">
-        <MainInfo
-          iconCode={currentWeather.weather[0].icon}
-          city={currentWeather.name}
-          country={timezone.countryName}
-          temperature={currentWeather.main.temp}
-        />
-        <HourlyWeather weather={dailyWeather.hourly} time={timezone} />
-        <AdditionalInfo
-          cloudiness={currentWeather.clouds.all}
-          pressure={currentWeather.main.pressure}
-          wind={currentWeather.wind.speed}
-          humidity={currentWeather.main.humidity}
-        />
-      </div>
-    );
-  }
-
-  return null;
 };
