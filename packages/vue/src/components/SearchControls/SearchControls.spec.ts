01241f54654b0435ec33728b6e3c66bc0523263a
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-07-06 19:45:07 +0200

    vue: add tests to SearchControls

--- packages/vue/src/components/SearchControls/SearchControls.spec.ts
+++ packages/vue/src/components/SearchControls/SearchControls.spec.ts
@@ -2,17 +2,35 @@ import { render, screen } from "@testing-library/vue";
 import userEvent from "@testing-library/user-event";
 import SearchControls from "./SearchControls.vue";
 
-const mockedSearchFunc = jest.fn(_ => Promise.resolve());
-const mockedSearchByLocationFunc = jest.fn(() => Promise.resolve());
-
-test("should not trigger search func when input is empty", async () => {
-  render(SearchControls, {
-    props: {
-      search: mockedSearchFunc,
-      searchByLocation: mockedSearchByLocationFunc,
-    },
-  });
+test("should emit empty string when input is empty", async () => {
+  const wrapper = render(SearchControls);
 
   userEvent.click(screen.getByRole("button", { name: /search search/i }));
-  expect(mockedSearchFunc).not.toBeCalled();
+  expect(wrapper.emitted().search[0]).toEqual([""]);
+});
+
+test("should emit input value when input is not empty", () => {
+  const wrapper = render(SearchControls);
+
+  const searchInput = screen.getByLabelText(/search/i);
+
+  //by pressing button
+  userEvent.type(searchInput, "Tokyo");
+  userEvent.click(screen.getByRole("button", { name: /search search/i }));
+  expect(wrapper.emitted().search).toBeTruthy();
+  expect(wrapper.emitted().search[0]).toEqual(["Tokyo"]);
+
+  userEvent.clear(searchInput);
+
+  //by hitting enter
+  userEvent.type(searchInput, "Moscow{enter}");
+  expect(wrapper.emitted().search).toBeTruthy();
+  expect(wrapper.emitted().search[1]).toEqual(["Moscow"]);
+});
+
+test("should emit to search by location when location button is pressed", () => {
+  const wrapper = render(SearchControls);
+
+  userEvent.click(screen.getByRole("button", { name: /location-search/i }));
+  expect(wrapper.emitted()["search-by-location"]).toBeTruthy();
 });
